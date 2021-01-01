6bb12b6ea17f35a327f3ba57b630cffdfce54ff6
siemen <siemensubbaiah1@gmail.com>
2021-01-01 23:43:17 +0530

    amstrong number

--- script2.js
+++ script2.js
@@ -68,3 +68,19 @@ const evenOddSums = (arr) => {
   // console.log(ans);
 };
 // evenOddSums([50, 60, 60, 45, 71]);
+
+//AMSTRONG NUMBER!
+const checkAmstrongNumber = (n) => {
+  const number = n.toString().split('');
+  let sum = 0;
+  let ans;
+  number.forEach((numb) => {
+    const check = numb * numb * numb;
+    ans = sum += check;
+  });
+  ans === n
+    ? console.log('it is an amstrong number!')
+    : console.log('it is not an amstrong number!');
+};
+
+checkAmstrongNumber(153);
